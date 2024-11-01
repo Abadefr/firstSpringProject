b53bcfcbde5f1ef9bcb0b0e452e68e3171309916
franklinux <universomacroexpande@gmail.com>
2024-11-01 15:28:55 -0300

    Initial commit for new repository

---
+++ src/main/java/com/EmployeerAplication/EmployeerAplication/EmployeerAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.EmployeerAplication.EmployeerAplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeerAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeerAplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EmployeerAplication/EmployeerAplication/EmployeerAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EmployeerAplication.EmployeerAplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeerAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
